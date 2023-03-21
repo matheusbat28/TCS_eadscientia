caa030ea738230bb4c125629a5dcb78eef11c53e
MATHEUS BATISTA <matheus.batista1@alunos.sc.senac.br>
2023-03-21 20:34:29 -0300

    ajuste no local do recaptcha

--- conta/forms.py
+++ conta/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import Usuario
 from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Checkbox
+from captcha.widgets import ReCaptchaV2Invisible, ReCaptchaV2Checkbox, ReCaptchaV3
 
 class UsuarioForm(forms.ModelForm):
     matricula = forms.CharField(widget=forms.TextInput(attrs={'id': 'matricula', 'placeholder': 'matrícula'}))
     senha = forms.CharField(widget=forms.PasswordInput(attrs={'id': 'senha', 'placeholder': 'senha'}))
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox(attrs={'data-theme': 'dark'}))
     class Meta:
         model = Usuario
         fields = ['matricula','senha']
