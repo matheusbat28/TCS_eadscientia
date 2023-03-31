0aff55ae1a0f55ed97e9690d5c47886ed945b53b
matheusbat28 <matheusarrow28@gmail.com>
2023-03-31 11:22:28 -0300

    inicio do menu do html home

--- vendor/js/home/scripts.js
+++ vendor/js/home/scripts.js
@@ -1,25 +1,11 @@
-'use strict'
+$(document).ready(function () {
+    $('$expandir-menu').click(function () {
+        if ($('#cbx-menu').is(':checked')) {
+            $('#cbx-menu').prop('checked', false);
+        } else {
+        }
 
-const menuSize = '250px';
 
-let open = true;
+    });
 
-document.querySelector('#btnMenu').addEventListener('click', e => {
-    open = !open;
-    toggleMenu();
-})
-
-document.querySelector('#btnClose').addEventListener('click', e => {
-    open = false;
-
-    toggleMenu();
-})
-
-function toggleMenu() {
-    if (open) {
-        document.querySelector('#menu').style.marginLeft = 0;
-        return;
-    }
-
-    document.querySelector('#menu').style.marginLeft = `${menuSize}`;
-}
+});
