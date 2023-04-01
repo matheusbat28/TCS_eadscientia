04b36de5e3f5f391cfabd0da8503144b196d0a3a
matheusbat28 <matheusarro28@gmail.com>
2023-04-01 02:01:13 -0300

    inicio do home

--- vendor/js/home/scripts.js
+++ vendor/js/home/scripts.js
@@ -1,27 +1,21 @@
 $(document).ready(function () {
 
-    $('#menu-rodape').click(function () {
+    function horario() {
+        var data = new Date();
+        var hora = data.getHours();
+        var minuto = data.getMinutes();
 
-        if ($('#cbx-menu').is(':checked')) {
-            $('#menu-rodape').css('justify-content', 'center');
-            $('#menu-rodape i').css('margin-right', '0');
-            $('#menu-rodape i').css('rotate', '0deg');
-            $('#logo-menu').css('justify-content', 'center');
-            $('#logo-menu').css('margin-left', '0');
-            $('#menu-conteudo a').css('justify-content', 'center');
-            $('#menu-conteudo a').css('margin-left', '0');
-            $('#menu-conteudo a h3').css('display', 'none');
-        } else {
-            $('#menu-rodape').css('justify-content', 'flex-end');
-            $('#menu-rodape i').css('margin-right', '40px');
-            $('#menu-rodape i').css('rotate', '180deg',);
-            $('#logo-menu').css('justify-content', 'flex-start');
-            $('#logo-menu').css('margin-left', '10px');
-            $('#menu-conteudo a').css('justify-content', 'flex-start');
-            $('#menu-conteudo a').css('margin-left', '20px');
-            $('#menu-conteudo a h3').css('display', 'block');
-
+        if (hora < 10) {
+            hora = "0" + hora;
         }
-    });
+        if (minuto < 10) {
+            minuto = "0" + minuto;
+        }
 
+        var horario = hora + ":" + minuto
+        $("time").html(horario);
+    }
+
+    horario();
+    setInterval(horario, 1000);
 });
