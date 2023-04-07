1264edcd6428a19c36534ba9f5fdedc038e3cea6
matheusbat28 <matheusarro28@gmail.com>
2023-04-07 16:36:24 -0300

    ajuste nas caixa de itens

--- vendor/js/menu/scripts.js
+++ vendor/js/menu/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
 
+    let admStatus = true;
+    let rhStatus = true;
+
+    $('.itens-subconteudo, .seta-baixo').hide();
+
     $('#menu-rodape').click(function () {
 
         if ($('#cbx-menu').is(':checked')) {
@@ -11,6 +16,9 @@ $(document).ready(function () {
             $('#menu-conteudo a').css('justify-content', 'center');
             $('#menu-conteudo a').css('margin-left', '0');
             $('#menu-conteudo a h3').css('display', 'none');
+            $('.itens-subconteudo').hide();
+            $('.seta-baixo').hide();
+            $('.seta-baixo').css('transform', 'rotate(0deg)');
         } else {
             $('#menu-rodape').css('justify-content', 'flex-end');
             $('#menu-rodape i').css('margin-right', '40px');
@@ -20,7 +28,31 @@ $(document).ready(function () {
             $('#menu-conteudo a').css('justify-content', 'flex-start');
             $('#menu-conteudo a').css('margin-left', '20px');
             $('#menu-conteudo a h3').css('display', 'block');
+            $('.seta-baixo').show();
+        }
+    });
 
+    $('#Administracao').click(function () {
+        if (admStatus && $('#cbx-menu').is(':checked')) {
+            $('#administracao-conteudo').show();
+            $('#seta-adm').css('transform', 'rotate(180deg)');
+            admStatus = false;
+        } else {
+            $('#administracao-conteudo').hide();
+            $('#seta-adm').css('transform', 'rotate(0deg)');
+            admStatus = true;
+        }
+    });
+
+    $('#rh').click(function () {
+        if (rhStatus && $('#cbx-menu').is(':checked')) {
+            $('#rh-conteudo').show();
+            $('#seta-rh').css('transform', 'rotate(180deg)');
+            rhStatus = false;
+        } else {
+            $('#rh-conteudo').hide();
+            $('#seta-rh').css('transform', 'rotate(0deg)');
+            rhStatus = true;
         }
     });
 
