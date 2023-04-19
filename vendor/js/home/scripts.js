690e231f70ad63b33f37114181a239a3df7a4235
matheusbat28 <matheusarrow28@gmail.com>
2023-04-18 23:48:10 -0300

    silider do carrocel

--- vendor/js/home/scripts.js
+++ vendor/js/home/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
 
+    $('.seta').click(function (e) {
+
+
+        if (e.target.children[0].id === 'seta-direta') {
+            $('#carrocel-img').children().first().appendTo('#carrocel-img');
+        } else if (e.target.children[0].id === 'seta-esquerda') {
+            $('#carrocel-img').children().last().prependTo('#carrocel-img');
+            console.log($('#carrocel-img').children().last());
+        }
+    });
 });
