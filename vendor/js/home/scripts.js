20414e49b6ec92f5b598c269dc385e8b94408575
Gabriel <oening700@gmail.com>
2023-04-22 03:21:47 -0300

    inicio animação carrossel

--- vendor/js/home/scripts.js
+++ vendor/js/home/scripts.js
@@ -9,3 +9,29 @@ $(document).ready(function () {
         }
     });
 });
+
+function slideshow() {
+  // pegar a primeira imagem do carrossel
+  const primeiraImg = document.querySelector('.carrocel-img:first-child');
+  // obter a largura da imagem para calcular o deslocamento
+  const imgWidth = primeiraImg.offsetWidth;
+
+  // mover o carrossel para a esquerda
+  document.querySelector('#carrocel-img').style.transform = `translateX(-${imgWidth}px)`;
+
+  // mover a primeira imagem para o final do carrossel
+  document.querySelector('#carrocel-img').appendChild(primeiraImg);
+
+  // reiniciar a posição do carrossel quando chegar ao final
+  const ultimaImg = document.querySelector('.carrocel-img:last-child');
+  if (ultimaImg.style.transform == `translateX(-${imgWidth}px)`) {
+    document.querySelector('#carrocel-img').style.transform = 'translateX(0)';
+  }
+}
+
+// chamar a função slideshow a cada 3 segundos
+setInterval(slideshow, 3000);
+
+
+
+
