9f1abd7d7355d0818dd6c334faf78cec26e9fc09
matheusbat28 <matheusarrow28@gmail.com>
2023-05-10 10:22:57 -0300

    ajuste na modal do login e impl da tela de add curso

--- templates/static/js/login/scripts.js
+++ templates/static/js/login/scripts.js
@@ -1,6 +1,9 @@
+$('.carregando, #fundo_modal').hide();
+
+let modalStatus = false;
+
 $(document).ready(function () {
     $('#matricula').mask('0000000000');
-    $('.carregando').hide();
 
 
     $("#olho-senha").mousedown(function () {
@@ -9,21 +12,6 @@ $(document).ready(function () {
 
     $("#olho-senha").mouseup(function () {
         $("#senha").attr("type", "password");
-    });
-
-    $('#esqueci-senha').click(function () {
-        if ($('#cbx_modal').is(':checked')) {
-            $('#modal').removeClass('modal-mostrar');
-            $('#modal').addClass('modal-sair');
-        } else {
-            $('#modal').removeClass('modal-sair');
-            $('#modal').addClass('modal-mostrar');
-        }
-    });
-
-    $('#modal-x label').click(function () {
-        $('#modal').removeClass('modal-mostrar');
-        $('#modal').addClass('modal-sair');
     });
 
     $('#btn-entrar').click(function (e) {
@@ -52,4 +40,14 @@ $(window).on('unload', function () {
     $('#carregando-log').hide();
     $('#btn-recuperar-senha i').show();
     $('#carregando-senha').hide();
+});
+
+$(document).on('click', '#esqueci-senha label, #modal-x', function () {
+    if (modalStatus == false) {
+        $('#fundo_modal').show();
+        modalStatus = true;
+    } else {
+        $('#fundo_modal').hide();
+        modalStatus = false;
+    }
 });
